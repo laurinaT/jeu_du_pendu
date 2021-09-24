2d3e100a9e8ef91bbfb7a2398a0dbb09884399ff
laurinaT <laurinatoussaint@gmail.com>
2021-09-24 09:53:27 +0200

    page accueil, j,q,r

---
+++ js/jeu_du_pendu.js
@@ -0,0 +1,24 @@
+// __________Déclaration des variables________________________________________________________
+
+let choixAccueilJ = prompt("Bonjour, Bienvenue sur le jeu du pendu !\n\nVous pouvez taper :\n   \"j\" : pour jouer\n   \"r\" : pour afficher les règles \n   \"q\" : pour quitter le jeu \n");
+
+
+
+// __________Déclaration des fonctions________________________________________________________
+
+function  choixAccueil(reponseJoueur) {
+    if (reponseJoueur === "j") {
+        alert("Voici les règles du pendu :\nLe but du jeu est simple : vous devez deviner le mot qui se cache sous les tirets... \n Donnez une lettre et si la lettre est dans le mot, elle s'affichera.\n Sinon vous perdez un point.\n Vous avez sept points, devinez le mot avant la fin des 7 points et vous avez gagner !\n");
+    }
+    else if (reponseJoueur === "r") {
+        alert("Voici les règles du pendu :\nLe but du jeu est simple : vous devez deviner le mot qui se cache sous les tirets... \n Donnez une lettre et si la lettre est dans le mot, elle s'affichera.\n Sinon vous perdez un point.\n Vous avez sept points, devinez le mot avant la fin des 7 points et vous avez gagner !\n");
+    }
+    else {
+        alert("Oooh, vous pouvez donc fermer cette page... \n A bientôt !!!");
+    }
+}
+
+
+// __________Code Courant________________________________________________________
+
+choixAccueil(choixAccueilJ);// appel fonction avec variable choixAccueilJoueur
