f48eeb1db8c95976a30729908c629841e9a5f732
laurinaT <laurinatoussaint@gmail.com>
2021-09-25 10:11:55 +0200

    récupération de la lettre du joueur

--- js/jeu_du_pendu.js
+++ js/jeu_du_pendu.js
@@ -1,15 +1,14 @@
-// // __________Déclaration des variables________________________________________________________
+// __________Déclaration des variables________________________________________________________
 
 // let choixAccueilJ = prompt("Bonjour, Bienvenue sur le jeu du pendu !\n\nVous pouvez taper :\n   \"j\" : pour jouer\n   \"r\" : pour afficher les règles \n   \"q\" : pour quitter le jeu \n");
 
 let motChoisi = listeMotsRandom(); // renvoi le mot choisi par la fonction
+let motBrouille = motCache(motChoisi);// récupère le mot brouille par des tirets renvoyé par la fonction motCache()
 
-
-// // __________Déclaration des fonctions________________________________________________________
+// __________Déclaration des fonctions________________________________________________________
 
 // function  choixAccueil(reponseJoueur) {
 //     if (reponseJoueur === "j") {
-//         alert("Voici les règles du pendu :\nLe but du jeu est simple : vous devez deviner le mot qui se cache sous les tirets... \n Donnez une lettre et si la lettre est dans le mot, elle s'affichera.\n Sinon vous perdez un point.\n Vous avez sept points, devinez le mot avant la fin des 7 points et vous avez gagner !\n");
 //     }
 //     else if (reponseJoueur === "r") {
 //         alert("Voici les règles du pendu :\nLe but du jeu est simple : vous devez deviner le mot qui se cache sous les tirets... \n Donnez une lettre et si la lettre est dans le mot, elle s'affichera.\n Sinon vous perdez un point.\n Vous avez sept points, devinez le mot avant la fin des 7 points et vous avez gagner !\n");
@@ -25,13 +24,14 @@ function listeMotsRandom() {// choisi un mot au hasard dans la liste
     return randomMot;
 }
 
-// function motCache (motChoisi) {
-    let splitMot = motChoisi.split('');// split le mot et le renvoi sous forme de tableau, ok fonctionne
+function motCache (mot) {
+    let splitMot = mot.split('');// split le mot et le renvoi sous forme de tableau, ok fonctionne
     let brouille = [];
     splitMot.forEach(lettre => brouille.push("_"));
-    alert(brouille);
-        
-// }
+    return brouille;// renvoie un tableau contenant des tirets au lieu des lettres du mot choisi
+}
+
 // __________Code Courant________________________________________________________
 
 // choixAccueil(choixAccueilJ);// appel fonction avec variable choixAccueilJoueur
+prompt(motBrouille + "\n\nChoisissez votre lettre : ");
