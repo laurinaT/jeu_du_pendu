ee418b45f4e895e33c91c7bb510c5dd77ed1e365
laurinaT <laurinatoussaint@gmail.com>
2021-09-26 09:26:43 +0200

    Démasquer bonne lettre du joueur

--- js/jeu_du_pendu.js
+++ js/jeu_du_pendu.js
@@ -3,6 +3,7 @@
 // let choixAccueilJ = prompt("Bonjour, Bienvenue sur le jeu du pendu !\n\nVous pouvez taper :\n   \"j\" : pour jouer\n   \"r\" : pour afficher les règles \n   \"q\" : pour quitter le jeu \n");
 
 let motChoisi = listeMotsRandom(); // renvoi le mot choisi par la fonction
+motChoisi = motChoisi.split(""); // split le mot et le renvoi sous forme de tableau, ok fonctionne
 let motBrouille = motCache(motChoisi);// récupère le mot brouille par des tirets renvoyé par la fonction motCache()
 let lettreJoueur = "";
 // __________Déclaration des fonctions________________________________________________________
@@ -25,24 +26,38 @@ function listeMotsRandom() {// choisi un mot au hasard dans la liste
 }
 
 function motCache (mot) {
-    let splitMot = mot.split('');// split le mot et le renvoi sous forme de tableau, ok fonctionne
+    console.log(mot);
     let brouille = [];
-    splitMot.forEach(lettre => brouille.push("_"));
+    mot.forEach(lettre => brouille.push("_"));
     return brouille;// renvoie un tableau contenant des tirets au lieu des lettres du mot choisi
 }
 
-function lettreJoueurConform(lettreJoueur) {
+function lettreJoueurConform(lettreJoueur) {// vérifie la longueur de lettre donnée par le joueur
     if(lettreJoueur.length === 1 ) {
         alert("OK !!!");
-        // return lettreJoueurOk;
+        return lettreJoueur.toLowerCase();
     }
     else{
         alert("Vous avez tapé trop de lettres... Ou pas assez, vous venez de perdre un point ^^");
     }
 }
 
+function verifAffichage(lettre, vraiMot, cache) { 
+    for(i = 0; i < vraiMot.length; i++) {
+        alert(vraiMot[i] + " _ " + lettre);
+
+        if (vraiMot[i] == lettre) {
+            cache[i] = lettre;
+        }
+    }
+    return cache;
+}
+
 // __________Code Courant________________________________________________________
 
 // choixAccueil(choixAccueilJ);// appel fonction avec variable choixAccueilJoueur
 lettreJoueur = prompt(motBrouille + "\n\nChoisissez votre lettre : ");
-lettreJoueurConform(lettreJoueur);
+lettreJoueur = lettreJoueurConform(lettreJoueur);
+motBrouille = verifAffichage(lettreJoueur, motChoisi, motBrouille);
+console.log(motBrouille);
+
